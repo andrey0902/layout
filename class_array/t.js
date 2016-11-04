7d3c63ec1827192c5406784612170d1e5ab457a6
andrey0902 <andreypavlov1706@gmail.com>
2016-11-04 15:24:26 +0200

    two solutions in element

--- class_array/t.js
+++ class_array/t.js
@@ -2,8 +2,6 @@ var obj = {
   className: 'open menu'
 }
 
-
-
 function addNewName(obj, name){
 	var str = obj.className.toLowerCase();
 		if(!~str.indexOf(name)){
@@ -13,11 +11,24 @@ function addNewName(obj, name){
 }
 
 addNewName(obj,'div');
-
 console.log(obj);
-addNewName(obj,'ffg');
 
-console.log(obj);
-addNewName(obj,'f5647g');
+function addNewElement(obj, name){
+	var mas = obj.className.split( ' ');
+	for ( var i=0; i<mas.length; i++){
+		if(mas[i]==name)return ;
+	}
+	mas.push(name);
+	
+	return obj.className =mas.join(' ');
+	
+}
+
+
+
+
+
+
+addNewElement(obj,'f5647g');
 
 console.log(obj);
